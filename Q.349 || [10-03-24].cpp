a4646da5f68eb8a6cfcaf89144b915d4286cc80e
Anshu Srivastava <100195276+Anshux31273@users.noreply.github.com>
2024-03-10 23:51:59 +0530

    Update Q.349 || [10-03-24].cpp
    Time: O(n + m)
    Space: O(n)

--- Q.349 || [10-03-24].cpp
+++ Q.349 || [10-03-24].cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> ans, ans2;
         vector<int> res;
-        unordered_map<int, int> u;
-        for(int i = 0;i < nums1.size();i ++)
+        unordered_map<int, int> n;
+        for(auto i : nums1) n[i] = 1;
+        for(auto i : nums2)
         {
-            ans.insert(nums1[i]);
+            if(n.find(i) != n.end())
+                n[i] = 2;
         }
-        for(int i = 0;i < nums2.size();i ++)
-        {
-            ans2.insert(nums2[i]);
-        }
-        for(auto i : ans)
-        {
-            u[i] ++;
-        }
-        for(auto i : ans2)
-        {
-            u[i] ++;
-        }
-        for(auto i : u)
-        {
-            if(i.second>1)
+        for(auto i : n)
+            if(i.second == 2)
                 res.push_back(i.first);
-        }
-
         return res;
-        
     }
 };
